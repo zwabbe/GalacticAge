f33b751c4b24b91c34fa09e66c20eb4b40b8b0d9
Jeremy Miller <gkstaff@gmail.com>
2021-02-10 13:58:58 -0800

    adds new commit.

--- __test__/spaceage.test.js
+++ __test__/spaceage.test.js
@@ -3,13 +3,28 @@ import GalaxyAge from '../src/js/spaceage.js';
 
 describe('GalaxyAge', () => {
 
-  let GalaxyAges;
+  let galacticAge;
 
   beforeEach(() => {
-    age = new GalaxyAge(10);
+    galacticAge = new GalaxyAge(32);
   });
 
   test('should return the age entered into field', () => {
-    expect(GalaxyAge.age).toEqual(10);
+    expect(galacticAge.age).toEqual(32);
   });
+  
+  test('should take galacticAge and modify it with Mercury equation', () => {
+    expect(galacticAge.Mercury()).toEqual(7);
+  });
+
+  test('should take age and modify it again the Venus equation', () => {
+    expect(galacticAge.Venus()).toEqual(19);
+  });
+  test('should take age and modify it against Mars() equation', () => {
+    expect(galacticAge.Mars()).toEqual(60);
+  });
+  test('should take age and modify it against Jupiter() equation', () => {
+    expect(galacticAge.Jupiter()).toEqual(379);
+  });
+
 });
